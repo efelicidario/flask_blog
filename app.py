c9283f10fb894232263702c5c70b962c9d5ae353
Tyler Felicidario <felicidario.ty@gmail.com>
2023-11-06 16:07:45 -0800

    added wtforms, namer form and route

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "fleeceKey"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("Enter your name: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Filters: |<filter name>
 #safe
@@ -40,5 +49,19 @@ def page_not_found(e):
 def page_not_found(e):
         return render_template("500.html"), 500
 
-if __name__ == "__main__":
-    app.run()
+# Create Name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+    return render_template("name.html",
+                           name = name,
+                           form = form)
+
+#if __name__ == "__main__":
+    #app.run()
